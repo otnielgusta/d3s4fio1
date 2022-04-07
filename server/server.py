2107e3bed30caffa126250722f986a15ec13fad1
Otniel Silva <55518288+otnielgusta@users.noreply.github.com>
2022-04-07 16:46:49 -0300

    VARIAVEIS DE AMBIENTE

--- server/server.py
+++ server/server.py
@@ -1,21 +1,18 @@
-import os
-import dotenv
 from flask import Flask
 from flask_restx import Api
 from mysql.connector import connection
 from dotenv import load_dotenv, find_dotenv
 from flask_cors import CORS
 
-load_dotenv(find_dotenv())
 
 
 class Server():
 
     mydb = connection.MySQLConnection(
-        host= os.getenv("HOST"),
-        user= os.getenv("USER"),
-        password=os.getenv("PASSWORD"),
-        database= os.getenv("DATABASE"),
+        host= "$HOST",
+        user="$USER",
+        password="$PASSWORD",
+        database= "$DATABASE",
         connection_timeout=60,
     )
     
@@ -26,7 +23,7 @@ class Server():
             "origins": "*"
         }
     })
-    
+
     api = Api(app)
     
     if __name__ == "__main__":
